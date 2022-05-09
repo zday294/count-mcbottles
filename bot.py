e430879a5f1ada26cc8e0fafc5baa5e58194c2ed
zday294 <zday294@students.bju.edu>
2022-05-09 19:03:09 -0400

    can now detect changes in voice channels

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 #bot.py
-from cmath import e
 import os
 import discord
 from dotenv import load_dotenv
@@ -11,8 +10,13 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 PREFIX = '-'
 
-pattern = ""
+class details:
+    def __init__(self, pat, monitored, replace):
+        self.pattern = pat
+        self.monitoredChannel = monitored
+        self.replaceChar = replace
 
+deets = details("", None, "#")
 
 bot = commands.Bot(PREFIX)
 
@@ -22,12 +26,30 @@ bot = commands.Bot(PREFIX)
 async def pattern(ctx, *pat: str):
     pat = str.join(' ', pat)
     pattern = pat
-    print(pattern)
+    print("Setting pattern")
+    print("\t", pattern)
+
+@bot.command(name="setchannel", help="Set the bot's monitored channel to the current channel of the invoking user")
+async def setchannel(ctx):
+    deets.monitoredChannel = ctx.author.voice.channel
+    print("Setting channel")
+    print("\t", ctx.message.author)
+    print("\t", ctx.author.voice.channel)
+    print("\t", deets.monitoredChannel)
 
 @bot.command(name='join', help="Join the voice channel the sending user is in")
 async def join(ctx):
-    
-    print(ctx.message.author)
+    # if(!message.member.voice.channel) return message.channel.send("Please connect to a voice channel!");
+    channel = ctx.author.voice.channel
+    print("Joining channel")
+    print("\t",ctx.message.author)
+    print("\t",ctx.author.voice.channel)
+    await channel.connect()
+
+@bot.command(name='leave', help="Banishes bot from the voice channel")
+async def leave(ctx):
+    print("leaving the vc")
+    await ctx.voice_client.disconnect()
 
 
 # EVENTS 
@@ -53,13 +75,39 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
+@bot.event
+async def on_voice_state_update(member, before, after):
+    print("Voice state update")
+    print("\t", member)
+    print("\tbefore.channel: ", before.channel)
+    print("\tafter.channel: ", after.channel)
+    print("\tmonitoredChannel: ", deets.monitoredChannel)
+    if (deets.monitoredChannel == None):
+        print("\tmonitoredChannel is null")
+    elif (after.channel == deets.monitoredChannel):
+        print("\tincrease number")
+    elif (before.channel == deets.monitoredChannel):
+        print("\tdecrease number")
+    else:
+        print("\thit the else")
+
 
 # UTILS
-async def get_users():
-    #get channel
-    #get size of members property
-    #https://stackoverflow.com/questions/66778544/get-count-of-users-in-a-voice-channel-discord-js
+async def getNumUsers():
+    # get channel
+    # get size of members property
+    # https://stackoverflow.com/questions/66778544/get-count-of-users-in-a-voice-channel-discord-js
+    for c in bot.get_all_channels:
+        if c.id == deets.monitoredChannel.id:
+            print("idk")
     print("not implemented")
+
+
+def updateChannelName():
+    numUsers = getNumUsers()
+    update = pattern.replace(deets.replaceChar, str(numUsers))
+    # update channel name here
+
 
 def main():
     try:
